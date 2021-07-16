500abcf4d11c6c97f77eb5c5a57037d247a3f4e7
Marcin Wosinek <marcin@wosinek.eu>
2021-07-16 08:53:39 +0200

    add build script

---
+++ build.js
@@ -0,0 +1,12 @@
+const vuePlugin = require("esbuild-vue");
+
+require("esbuild").build({
+  entryPoints: ["src/main.js"],
+  bundle: true,
+  outfile: "dist/main.js",
+  logLevel: "info",
+  plugins: [vuePlugin()],
+  define: {
+    "process.env.NODE_ENV": JSON.stringify("development"),
+  },
+});
